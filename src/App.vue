c83eacd8786a3e19afa821fbfcd6dc88d49fe30c
tim-labahn <tim.labahn@freaknet.de>
2023-08-23 12:18:29 +0200

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <dialog :class="'gameStart'"></dialog>
-  <div :class="'field'">
-    <div v-for="row in gameSize">
-      <div :class="'row'">
-        <div v-for="objects in gameSize">
-          <div :class="'field'">
-            {{ objects }}
-          </div>
-        </div>
+  <dialog class="GameStart" :open="openStart">
+    <p>
+      Click
+      <button :class="'StartGame'" @click="(openStart = false), game()">Here</button>
+      to start the game
+    </p>
+  </dialog>
+  <dialog class="GameEnd" :open="openRestart">
+    <p>
+      Click
+      <button :class="'RestartGame'" @click="(openRestart = false), game()">Here</button>
+      to restart the game
+    </p>
+  </dialog>
+  <div class="field">
+    <div class="row" v-for="row in gameMap">
+      <div
+        class="tile"
+        v-for="tile in row"
+        @click="
+          if (tile.isEmpty) {
+            (tile.isEmpty = false), (tile.isCross = player), checkWinLose(), playerSwitch();
+          }
+        "
+      >
+        <div>{{ tile.isCross }}</div>
       </div>
     </div>
   </div>
@@ -15,19 +32,62 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-const gameSize = ref<number>(3);
 
 type Tile = {
   isEmpty: boolean;
   isCross: string;
 };
 
+const gameSize = 3;
 const gameMap = ref<Tile[][]>([]);
+const openStart = ref(true);
+const openRestart = ref(false);
+const player = ref('✗');
+
+function game() {
+  player.value = '✗';
+  generateField();
+}
+
+function checkWinLose() {
+  const players = ['✗', '◯'];
+  for (let i = 0; i < 3; i++) {
+    for (const player of players)
+      if (
+        (gameMap.value[i][0].isCross === player && gameMap.value[i][1].isCross === player && gameMap.value[i][2].isCross === player) ||
+        (gameMap.value[0][i].isCross === player && gameMap.value[1][i].isCross === player && gameMap.value[2][i].isCross === player) ||
+        (gameMap.value[0][0].isCross === player && gameMap.value[1][1].isCross === player && gameMap.value[2][2].isCross === player) ||
+        (gameMap.value[0][2].isCross === player && gameMap.value[1][1].isCross === player && gameMap.value[2][0].isCross === player)
+      ) {
+        openRestart.value = true;
+      } else if (
+        !gameMap.value[0][0].isEmpty &&
+        !gameMap.value[0][1].isEmpty &&
+        !gameMap.value[0][2].isEmpty &&
+        !gameMap.value[1][0].isEmpty &&
+        !gameMap.value[1][1].isEmpty &&
+        !gameMap.value[1][2].isEmpty &&
+        !gameMap.value[2][0].isEmpty &&
+        !gameMap.value[2][1].isEmpty &&
+        !gameMap.value[2][2].isEmpty
+      ) {
+        openRestart.value = true;
+      }
+  }
+}
+function playerSwitch() {
+  if (player.value === '✗') {
+    player.value = '◯';
+  } else {
+    player.value = '✗';
+  }
+}
 
 function generateField() {
-  for (let i = 0; i < 3; i++) {
+  gameMap.value = [];
+  for (let i = 0; i < gameSize; i++) {
     const rowY = [];
-    for (let k = 0; k < 3; k++) {
+    for (let k = 0; k < gameSize; k++) {
       const tile = {
         isEmpty: true,
         isCross: '',
@@ -44,19 +104,24 @@ function generateField() {
 .tile {
   border: 0.5px solid black;
   box-sizing: border-box;
-  width: 50px;
-  height: 50px;
+  width: 100px;
+  height: 100px;
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 50px;
+  font-size: 80px;
+  user-select: none;
 }
 
 .field {
   display: grid;
-  grid-template-columns: (9, 1fr);
-  width: 150px;
+  grid-template-columns: repeat(3, 1fr);
+  width: 300px;
 
   /* margin-right: 15.4%; */
+}
+
+.StartGame {
+  height: 45px;
 }
 </style>
